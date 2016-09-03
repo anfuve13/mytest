22ef947d134ed682b758e58cb0cd12443c729a18
anfuve13 <anfuve.13@gmail.com>
2016-09-03 06:09:40 +0600

    another day in paradise

---
+++ afile.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define m_p make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long
+#define l_b lower_bound
+#define u_b upper_bound
+
+int main()
+{
+	cout<<"HAPPY WORLD"<<endl;
+}
